97922d9306d886e13d648f82f6115e9647a2aae7
just nobody <kingdaro@gmail.com>
2016-12-29 06:57:30 -0500

    restructure

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express = require('express')
 const app = express()
 const port = +process.env.PORT || 8080
 
-app.use(express.static('static'))
+app.use('/styles', express.static('styles'))
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 app.listen(port, () => {
   console.log(`Server listening on localhost:${port}`)
