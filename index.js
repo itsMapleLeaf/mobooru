50cf600108da22d84848722143d5302b83cc88c4
just nobody <kingdaro@gmail.com>
2016-12-29 09:34:28 -0500

    toss the test images

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 
-const app = express()
 const port = +process.env.PORT || 8080
 
-const images = fs.readdirSync(path.resolve(__dirname, './images'))
+const app = express()
 
 app.use('/styles', express.static('styles'))
 app.use('/scripts', express.static('scripts'))
-app.use('/images', express.static('images'))
 
 app.set('view engine', 'pug')
 
@@ -18,14 +16,14 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/images', (req, res) => {
-  res.send({ images: images.slice(0, 3) })
+  // res.send({ images: images.slice(0, 3) })
 })
 
 app.get('/api/images/:previous', (req, res) => {
-  const index = images.indexOf(req.params.previous)
-  const start = index + 1
-  const end = Math.min(index + 4, images.length)
-  res.send({ images: images.slice(start, end) })
+  // const index = images.indexOf(req.params.previous)
+  // const start = index + 1
+  // const end = Math.min(index + 4, images.length)
+  // res.send({ images: images.slice(start, end) })
 })
 
 app.listen(port, () => {
