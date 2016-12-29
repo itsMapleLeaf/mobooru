efd260ac3118fc8f99f8eaea371c164f73684db8
just nobody <kingdaro@gmail.com>
2016-12-29 07:25:16 -0500

    rendering images dynamically

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 
 const app = express()
 const port = +process.env.PORT || 8080
 
+const images = fs.readdirSync(path.resolve(__dirname, './images'))
+
 app.use('/styles', express.static('styles'))
+app.use('/images', express.static('images'))
 
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { images })
 })
 
 app.listen(port, () => {
