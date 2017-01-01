7d62300ad1123fd43bdcdadaef3cb8cffa313125
just nobody <kingdaro@gmail.com>
2017-01-01 10:03:47 -0500

    added some watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,7 @@ const webpackConfig = {
   }
 }
 
+
 gulp.task('build:server', () =>
   gulp.src('src/server/**/*.ts')
     .pipe(tsProject())
@@ -39,5 +40,18 @@ gulp.task('build:web:webpack', done => webpack(webpackConfig, done))
 gulp.task('build:web', ['build:web:pug', 'build:web:webpack'])
 
 gulp.task('build', ['build:server', 'build:web'])
+
+
+gulp.task('watch:server', () => {
+  gulp.watch('src/server/**/*.ts', ['build:server'])
+})
+
+gulp.task('watch:web', () => {
+  gulp.watch('src/web/**/*.pug', ['build:web:pug'])
+  gulp.watch('src/web/(components/**/*|styles/**/*|main.js)', ['build:web:webpack'])
+})
+
+gulp.task('watch', ['build', 'watch:server', 'watch:web'])
+
 
 gulp.task('default', ['build'])
