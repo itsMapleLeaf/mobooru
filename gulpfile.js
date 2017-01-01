fd7d571dcbbe037240cd2948d79efca50da23ffa
just nobody <kingdaro@gmail.com>
2017-01-01 09:29:10 -0500

    holy shit it worked

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,25 @@
 const gulp = require('gulp')
+const pug = require('gulp-pug')
 const ts = require('gulp-typescript')
+const webpack = require('webpack')
+const path = require('path')
+
 const tsProject = ts.createProject('tsconfig.json')
+
+const webpackConfig = {
+  entry: path.resolve(__dirname, 'src/web/main.js'),
+  output: {
+    path: path.resolve(__dirname, 'build/web'),
+    filename: 'main.js',
+    publicPath: '/build/web/',
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.vue$/, loader: 'vue-loader' },
+    ]
+  }
+}
 
 gulp.task('build:server', () =>
   gulp.src('src/server/**/*.ts')
@@ -8,6 +27,16 @@ gulp.task('build:server', () =>
     .pipe(gulp.dest('build/server'))
 )
 
-gulp.task('build', ['build:server'])
+gulp.task('build:web:pug', () =>
+  gulp.src('src/web/**/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('build/web'))
+)
+
+gulp.task('build:web:webpack', done => webpack(webpackConfig, done))
+
+gulp.task('build:web', ['build:web:pug', 'build:web:webpack'])
+
+gulp.task('build', ['build:server', 'build:web'])
 
 gulp.task('default', ['build'])
