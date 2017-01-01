2ae0e8ae667e62418b142cee1129c95e8a5cc5e2
just nobody <kingdaro@gmail.com>
2017-01-01 12:29:51 -0500

    images stress test

---
+++ web/images.js
@@ -0,0 +1,15 @@
+const images = Array(9999)
+
+const imageTemplate = document.querySelector('#mb-imageTemplate')
+const imageListElement = document.querySelector('.mb-imageList')
+
+function renderImages() {
+  imageListElement.innerHTML = ''
+
+  for (const src of images) {
+    const el = document.importNode(imageTemplate.content, true)
+    imageListElement.appendChild(el)
+  }
+}
+
+renderImages()
