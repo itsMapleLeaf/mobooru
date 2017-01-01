5e49e97b922415c15f63c3cae8984edb244825ce
just nobody <kingdaro@gmail.com>
2017-01-01 12:33:12 -0500

    use an iife

--- web/images.js
+++ web/images.js
@@ -1,15 +1,16 @@
-const images = Array(9999)
+(function() {
+  const images = []
 
-const imageTemplate = document.querySelector('#mb-imageTemplate')
-const imageListElement = document.querySelector('.mb-imageList')
+  const imageTemplate = document.querySelector('#mb-imageTemplate')
+  const imageListElement = document.querySelector('.mb-imageList')
 
-function renderImages() {
-  imageListElement.innerHTML = ''
-
-  for (const src of images) {
-    const el = document.importNode(imageTemplate.content, true)
-    imageListElement.appendChild(el)
+  function renderImages() {
+    imageListElement.innerHTML = ''
+    for (const src of images) {
+      const el = document.importNode(imageTemplate.content, true)
+      imageListElement.appendChild(el)
+    }
   }
-}
 
-renderImages()
+  renderImages()
+})()
