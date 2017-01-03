41c44c930cc798c1504ebdcfece354accb7aaec2
just nobody <kingdaro@gmail.com>
2017-01-03 13:37:49 -0500

    fix addEventListener usage

--- web/scripts/overlay.js
+++ web/scripts/overlay.js
@@ -1,7 +1,7 @@
 const overlays = document.querySelectorAll('.mb-overlay')
 
 for (const overlay of overlays) {
-  overlay.addEventListener(event => {
+  overlay.addEventListener('click', event => {
     if (event.target === overlay) {
       event.preventDefault()
       overlay.classList.remove('mb-overlay--visible')
