8307320bf880dac0b7ee36fc5c7a23f246e4934c
just nobody <kingdaro@gmail.com>
2017-01-03 16:01:12 -0500

    hi aoba

--- web/scripts/images.js
+++ web/scripts/images.js
@@ -1,14 +1,23 @@
-const images = Array(10).fill(null)
+const images = []
 
 const imageTemplate = document.querySelector('#mb-imageTemplate')
 const imageListElement = document.querySelector('.mb-imageList')
 
-function renderImages() {
+function fetchImages() {
+  window.fetch('/api/images')
+    .then(res => res.json())
+    .then(res => (images = images.concat(res.images)))
+    .then(render)
+}
+
+function render() {
   imageListElement.innerHTML = ''
-  for (const src of images) {
+  for (const id of images) {
     const node = document.importNode(imageTemplate.content, true)
+    const link = node.querySelector('a')
+    link.style.backgroundImage = `url(/api/image/${id})`
     imageListElement.appendChild(node)
   }
 }
 
-renderImages()
+fetchImages()
