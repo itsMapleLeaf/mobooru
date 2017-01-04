0dffaeabaa6f6752e75a378974b1483ccbbca3b0
just nobody <kingdaro@gmail.com>
2017-01-04 12:09:17 -0500

    move button/input styles to common.sccss

--- web/app/main.js
+++ web/app/main.js
@@ -1,10 +1,11 @@
-import Vue from 'vue'
-import App from './components/App.vue'
-
 import 'normalize.css/normalize.css'
 import 'mdi/css/materialdesignicons.min.css'
 
 import './styles/base.scss'
+import './styles/common.scss'
+
+import Vue from 'vue'
+import App from './components/App.vue'
 
 new Vue({
   el: 'main',
