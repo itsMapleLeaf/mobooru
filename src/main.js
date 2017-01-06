41b1aecae0cc090129ac749ed59ee707970278d4
just nobody <kingdaro@gmail.com>
2017-01-06 18:11:05 -0500

    init firebase

--- src/main.js
+++ src/main.js
@@ -7,12 +7,23 @@ import './styles/transitions.scss'
 
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import * as firebase from 'firebase'
+
 import Index from './routes/Index.vue'
 import Upload from './routes/Upload.vue'
 import Image from './routes/Image.vue'
 
-Vue.use(VueRouter)
+// init firebase
+firebase.initializeApp({
+  apiKey: 'AIzaSyCaHZ0GElyOmXKkc-LeEdD1FNtL1uxufjY',
+  authDomain: 'mobooru-27d7f.firebaseapp.com',
+  databaseURL: 'https://mobooru-27d7f.firebaseio.com',
+  storageBucket: 'mobooru-27d7f.appspot.com',
+  messagingSenderId: '656274868388'
+})
 
+// init vue router
+Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
     // { path: '/', component: Index },
