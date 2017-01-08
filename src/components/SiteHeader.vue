b7f85dc9644e98381f1e02e67e04ff00a7fcfdd6
just nobody <kingdaro@gmail.com>
2017-01-07 19:54:51 -0500

    move header stuff into header, + labels

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,8 +1,48 @@
 <template>
   <header class="header">
-    <slot></slot>
+    <h2 class="header-title header-item">
+      <router-link to="/">mobooru</router-link>
+    </h2>
+    <input class="mb-input header-item header-item--stretch" placeholder="Search...">
+    <button class="mb-button header-item">
+      <icon>magnify</icon>
+    </button>
+
+    <template v-if="user">
+      <router-link to="/upload" class="mb-button header-item">
+        <icon>upload</icon>
+        <span class="header-item-label">Upload</span>
+      </router-link>
+      <router-link to="/signout" class="mb-button header-item">
+        <icon>logout</icon>
+        <span class="header-item-label">Sign Out</span>
+      </router-link>
+    </template>
+
+    <template v-else>
+      <router-link to="/login" class="mb-button header-item">
+        <icon>login</icon>
+        <span class="header-item-label">Sign In</span>
+      </router-link>
+    </template>
   </header>
 </template>
+
+<script>
+import * as firebase from 'firebase'
+
+export default {
+  components: {
+    Icon: require('../components/Icon.vue'),
+  },
+  data() {
+    return { user: null }
+  },
+  created() {
+    firebase.auth().onAuthStateChanged(user => { this.user = user })
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 @import '../styles/mixins';
@@ -33,6 +73,12 @@
 
 .header-title {
   @media (max-width: 480px) {
+    display: none;
+  }
+}
+
+.header-item-label {
+  @media (max-width: 800px) {
     display: none;
   }
 }
--- src/views/App.vue
+++ src/views/App.vue
@@ -1,30 +1,7 @@
 <template>
   <main>
     <section class="container">
-      <site-header class="container-item">
-        <h2 class="header-title header-item">
-          <router-link to="/">mobooru</router-link>
-        </h2>
-        <input class="mb-input header-item header-item--stretch" placeholder="Search...">
-        <button class="mb-button header-item">
-          <icon>magnify</icon>
-        </button>
-
-        <template v-if="user">
-          <router-link to="/upload" class="mb-button header-item">
-            <icon>upload</icon>
-          </router-link>
-          <router-link to="/signout" class="mb-button header-item">
-            <icon>logout</icon>
-          </router-link>
-        </template>
-
-        <template v-else>
-          <router-link to="/login" class="mb-button header-item">
-            <icon>login</icon>
-          </router-link>
-        </template>
-      </site-header>
+      <site-header class="container-item"></site-header>
       <image-list class="container-item container-item--stretch" :images='images'></image-list>
     </section>
     <router-view></router-view>
@@ -32,7 +9,6 @@
 </template>
 
 <script>
-import * as firebase from 'firebase'
 import * as store from '../store'
 
 export default {
@@ -43,11 +19,9 @@ export default {
     SiteHeader: require('../components/SiteHeader.vue'),
   },
   data: () => ({
-    user: null,
     images: [],
   }),
   async created() {
-    firebase.auth().onAuthStateChanged(user => { this.user = user })
     this.images = await store.fetchImageList()
   }
 }
